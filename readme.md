cdb6e0c3ec704ffebde0f7487e29f95bd198856a
stupot2dstudy <stupot2dstudy@gmail.com>
2023-11-14 20:05:42 +0000

    full update

---
+++ readme.md
@@ -0,0 +1,49 @@
+# Star Wars Random Character
+
+This project is a simple web application that allows users to generate random Star Wars character profiles using the Star Wars API.
+
+## Features
+
+Random Character Generation: Click a button to fetch and display a random Star Wars character's profile.
+
+Profile Display: Displays the character's name, homeworld, species, and an image (if available).
+
+Fallback Image: Provides a default image if the character's image is not available.
+
+## Technologies Used
+
+HTML
+CSS
+JavaScript
+Star Wars API (Link to the API)
+
+## How to Use
+
+1- Clone the repository to your local machine.
+
+2- Open index.html in your web browser.
+
+3- Click the "StarWars Random Character" button to generate a random character profile.
+
+## Code Structure
+
+index.html: Contains the basic HTML structure of the web page.
+
+style.css: Stylesheet for the layout and design of the web page.
+
+script.js: JavaScript file handling API calls and profile generation.
+
+## Installation
+
+There are no specific installation steps required. Simply download or 
+clone the repository and open index.html in a web browser.
+
+## Credits
+
+@stupot2d: Creator of this application.
+
+## Future Improvements
+
+Additional Styling: Enhance the visual design to further resemble the Star Wars theme.
+
+Error Handling: Implement better error handling for API failures.
--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-const names = document.getElementById("names");
-const homeworld = document.getElementById("planet");
-const gender = document.getElementById("gender");
-const button = document.querySelector(".ramdonmizeButton");
+function chooseYourCharacter(character) {
+    const nameElement = document.querySelector('.main-container-profile-content-name');
+    const homeworldElement = document.querySelector('.main-container-profile-content-homeworld');
+    const speciesElement = document.querySelector('.main-container-profile-content-species');
+    const imageElement = document.querySelector('.main-container-profile-img');
 
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  names.innerHTML = "<em>Loading...</em>";
-  planet.innerHTML = "<em>Loading...</em>";
-  gender.innerHTML = "<em>Loading...</em>";
-  image.innerHTML = "<em>Loading...</em>";
+    nameElement.textContent = `Name: ${character.name}`;
+    homeworldElement.textContent = `Homeworld: ${character.homeworld}`;
+    speciesElement.textContent = `Species: ${character.species}`;
 
-  const randomNumber = Math.ceil(Math.random() * 83);
-  fetch(`https://akabab.github.io/starwars-api/api/id/${randomNumber}.json`)
-    .then((response) => response.json())
-    .then((character) => {
-      console.log(character);
-      names.innerHTML = character.name;
-      planet.innerHTML = character.homeworld;
-      gender.innerHTML = character.gender;
-      image.innerHTML = `<img src = ${character["image"]} height = 100% width = 100%/>`;
-    });
+    if (character.image && character.image.trim() !== '') {
+        imageElement.src = character.image;
+    } else {
+        // Set a default image if the character doesn't have an image
+        imageElement.src = 'images/profile-picture.jpg'; // Replace 'profile-picture.jpg' with your default image path
+    }
+    imageElement.alt = character.name; // Set alt text for the image if applicable
+}
+
+function bringStarWarsChar(randomNumber) {
+    fetch(`https://akabab.github.io/starwars-api/api/id/${randomNumber}.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            if (data) {
+                chooseYourCharacter(data);
+            } else {
+                chooseYourCharacter({
+                    name: 'Not Found',
+                    homeworld: 'N/A',
+                    species: 'N/A',
+                    image: ''
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+}
+
+const showMe = document.querySelector('.header-container');
+showMe.addEventListener("click", (e) => {
+    e.preventDefault();
+    const randomNumber = Math.ceil(Math.random() * 83);
+    bringStarWarsChar(randomNumber);
 });
